77a3f44b6db148b6f196a5ff4bae9b1647904f6a
Armin Wessel <armin.wessel95@gmail.com>
2022-08-28 13:12:07 +0200

    Change of DH Parameters Change of calculation of a single transformation Renaming of some variables for consistency

--- src/dh_estimator.py
+++ src/dh_estimator.py
@@ -7,7 +7,7 @@ The model is based on the DH convention
 import json
 import rospy
 import numpy as np
-import math
+import math as m
 import matplotlib.pyplot as plt
 import cv2
 from std_msgs.msg import String
@@ -31,31 +31,66 @@ class DHestimator():
         """
         Constructor
         """
-
         self.sub_meas = rospy.Subscriber("meas", Meas, self.process_measurement)
         self.pub_est = rospy.Publisher("est", Est, queue_size=20)
 
-
-        
-
         # nominal DH parameters
           # nominal theta parameters are joint coors
-        self.theta_nom=np.array([0,np.pi,np.pi,0,np.pi/2,0,np.pi/2])
-        self.d_nom=np.array([0.1525,0.2025,0.2325,0.1825,0.2175,0.1825,0.081])
-        self.a_nom=np.array([0,0,0,0,0,0,0])
-        self.alpha_nom=np.array([0,np.pi/2,-np.pi/2,np.pi/2,np.pi/2,np.pi/2,-np.pi/2])
+        # self.theta_nom=np.array([0,np.pi,np.pi,0,np.pi/2,0,np.pi/2])
+        # self.d_nom=np.array([0.1525,0.2025,0.2325,0.1825,0.2175,0.1825,0.081])
+        # self.a_nom=np.array([0,0,0,0,0,0,0])
+        # self.alpha_nom=np.array([0,np.pi/2,-np.pi/2,np.pi/2,np.pi/2,np.pi/2,-np.pi/2])
+
+        # self.theta_nom =np.array([0,     0, 0,     0, 0,   0, 0    ])
+        self.theta_nom =np.array([0,     0, 0,     0, 0,   0, 0    ])
+        self.a_nom     =np.array([0.355, 0, 0.415, 0, 0.4, 0, 0.236])
+        self.d_nom     =np.array([0,     0, 0,     0, 0,   0, 0    ])
+        pip2 = np.pi/2
+        self.alpha_nom =np.array([0, pip2, -pip2, -pip2, pip2, pip2, -pip2])
 
         num_links = self.theta_nom.size
         self.rls=RLS(4*num_links,1) 
+        self.distances=np.zeros((0,))
 
-
+    def Rx(self, x):
+        return np.array([   [ 1, 0       , 0       , 0],
+                            [ 0, m.cos(x),-m.sin(x), 0],
+                            [ 0, m.sin(x), m.cos(x), 0],
+                            [ 0, 0       , 0       , 1]])
+  
+    def Ry(self, x):
+        return np.array([   [ m.cos(x), 0, m.sin(x), 0],
+                            [ 0       , 1, 0       , 0],
+                            [-m.sin(x), 0, m.cos(x), 0],
+                            [0        , 0, 0       , 1]])
+  
+    def Rz(self, x):
+        return np.array([   [ m.cos(x), -m.sin(x), 0, 0],
+                            [ m.sin(x), m.cos(x) , 0, 0],
+                            [ 0       , 0        , 1, 0],
+                            [ 0       , 0        , 0, 1]])
     
+    def Trans(self, x,y,z):
+        return np.array([   [ 1, 0, 0, x],
+                            [ 0, 1, 0, y],
+                            [ 0, 0, 1, z],
+                            [ 0, 0, 0, 1]])
+    def roundprint(self, H):
+        for line in H:
+            linestr=''
+            for elem in line:
+                elem = 0.001*round(1000*elem)
+                if (elem>=0):
+                    linestr += " {:.3f} ".format(elem)
+                else:
+                    linestr += "{:.3f} ".format(elem)
+            print(linestr+'\n')
+        print('\n')
+
     def get_T__i(self, theta__i, d__i, a__i, alpha__i) -> np.array:
-        t1 = math.cos(theta__i)
-        t2 = math.sin(theta__i)
-        t3 = math.cos(alpha__i)
-        t4 = math.sin(alpha__i)
-        return np.array([[t1,-t2 * t3,t2 * t4,t1 * a__i],[t2,t1 * t3,-t1 * t4,t2 * a__i],[0,t4,t3,d__i],[0,0,0,1]])
+        #T = self.Rz(theta__i)@self.Trans(0,0,d__i)@self.Trans(0,0,a__i)@self.Rx(alpha__i)
+        T = self.Rx(alpha__i)@self.Trans(d__i,0,0)@self.Rz(theta__i)@self.Trans(0,0,a__i)
+        return T
 
 
     def get_T_jk(self,j,k,theta_all, d_all, a_all, alpha_all) -> np.array:
@@ -90,8 +125,8 @@ class DHestimator():
             t__i_0=T__i_0[0:3,3]
             R__i_0=T__i_0[0:3,0:3]
             m__1i=np.array([[0],[0],[1]])
-            m__2i=np.array([[math.cos(theta_all[i-1])],[math.sin(theta_all[i-1])],[0]])
-            m__3i=np.array([[-d_all[i-1]*math.sin(theta_all[i-1])],[d_all[i-1]*math.cos(theta_all[i-1])],[0]])
+            m__2i=np.array([[m.cos(theta_all[i-1])],[m.sin(theta_all[i-1])],[0]])
+            m__3i=np.array([[-d_all[i-1]*m.sin(theta_all[i-1])],[d_all[i-1]*m.cos(theta_all[i-1])],[0]])
             _t1=np.matmul(R__i_0,m__1i)
             _t2=np.matmul(R__i_0,m__2i)
 
@@ -117,39 +152,41 @@ class DHestimator():
     
 
     def process_measurement(self, m):
-        # calculate the expected pose difference based on forward kinematics with nominal params
-        theta_nom1 = np.array(m.joints_neg).flatten()
-        theta_nom2 = np.array(m.joints_pos).flatten()
-        T_nom1 = self.get_T__i0(7,theta_nom1, self.d_nom, self.a_nom, self.alpha_nom)
-        T_nom2 = self.get_T__i0(7,theta_nom2, self.d_nom, self.a_nom, self.alpha_nom)
-        dtvec_nom = T_nom1[0:3,3].reshape((3,1)) - T_nom2[0:3,3].reshape((3,1))
-        drvec_nom = cv2.Rodrigues(T_nom1[0:3,0:3])[0] - cv2.Rodrigues(T_nom2[0:3,0:3])[0]
+        # calculate the poses of the camera based on the nominal 
+        # parameters and forward kinematics
+        num_links = (np.array(m.joints_neg)).size
+        theta_nom1 = np.array(m.joints_neg)
+        T_nom1 = self.get_T__i0(num_links,theta_nom1, self.d_nom, self.a_nom, self.alpha_nom)
+        tvec_nom1 = T_nom1[0:3,3].reshape((3,1))
+        rvec_nom1 = cv2.Rodrigues(T_nom1[0:3,0:3])[0]
 
-        # extract the measured pose differences
-        dtvec_real = np.reshape(np.array(m.dtvec),(3,1))
-        drvec_real = np.reshape(np.array(m.drvec),(3,1))
 
-        # calculate parameter jacobian for 0.5*((q+) + (q-))
-        # jacobian = self.get_parameter_jacobian(0.5*(theta_nom1+theta_nom2), self.d_nom, self.a_nom, self.alpha_nom)
+        theta_nom2 = np.array(m.joints_pos)
+        T_nom2 = self.get_T__i0(num_links,theta_nom2, self.d_nom, self.a_nom, self.alpha_nom)
+        tvec_nom2 = T_nom2[0:3,3].reshape((3,1))
+        rvec_nom2 = cv2.Rodrigues(T_nom2[0:3,0:3])[0]
 
+        # calculate the difference in the nominal poses
+        dtvec_nom = tvec_nom1 - tvec_nom2
+        drvec_nom = rvec_nom1 - rvec_nom2
+
+        # calculate the error between th expected and measured pose differenced
+        dtvec_real, drvec_real = np.reshape(np.array(m.dtvec),(3,1)), np.reshape(np.array(m.drvec),(3,1))
+        current_error=np.concatenate((dtvec_real-dtvec_nom,drvec_real-drvec_nom),axis=0)
+
+        # calculate the corresponding difference jacobian
         jacobian1 = self.get_parameter_jacobian(theta_nom1, self.d_nom, self.a_nom, self.alpha_nom)
         jacobian2 = self.get_parameter_jacobian(theta_nom2, self.d_nom, self.a_nom, self.alpha_nom)
         jacobian = jacobian1-jacobian2
 
-        # calculate errors between expected and measured pose difference
-        dtvec_error = dtvec_real-dtvec_nom
-        drvec_error = drvec_real-drvec_nom
-        current_error=np.concatenate((dtvec_error, drvec_error),axis=0)
-
-  
-        # use RLS to improve estimate of parameters
+        # use RLS
         self.rls.add_obs(S=jacobian, Y=current_error)
         estimate_k = self.rls.get_estimate().flatten()
 
+        # compose and publish message containing estimate
         msg = Est()
         msg.estimate = estimate_k
         msg.joints = theta_nom1
-
         self.pub_est.publish(msg)
 
 
@@ -163,6 +200,7 @@ class RLS():
         assert q <= 1 and q > 0.95, "q usually needs to be from ]0.95, 1]"
         self.q = q
 
+        self.alpha=alpha
         self.num_params = num_params
         self.P = alpha*np.eye(num_params) #initial value of matrix P
         self.phat = np.zeros((num_params,1)) #initial guess for parameters, col vector
@@ -197,6 +235,10 @@ class RLS():
 
             self.phat = self.phat + self.k*(y - s_T@self.phat)
             self.num_obs = self.num_obs+1
+        
+        # if (np.any(np.abs(self.phat)>0.15)): # values are too big, reset LSQ
+        #     self.P = self.alpha*np.eye(self.num_params) #initial value of matrix P
+        #     self.phat = np.zeros((self.num_params,1)) #initial guess for parameters, col vector
 
     def get_estimate(self):
         return self.phat
